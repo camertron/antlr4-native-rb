fcf976918c8688972045192dd437bdf3240fd1a1
Cameron Dutro <camertron@gmail.com>
2023-10-13 15:05:18 -0700

    Huh, weird

--- spec/run-lua-test.sh
+++ spec/run-lua-test.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-antlr_version=$(bundle exec ruby -rantlr4-native -e "puts Antlr4Native::Generator::ANTLR_VERSION")
+antlr_version=$(bundle exec ruby -Ilib -rantlr4-native -e "puts Antlr4Native::Generator::ANTLR_VERSION")
 docker build --build-arg ANTLR_VERSION="${antlr_version}" -t antlr4-native-rb:latest .
 docker run -t antlr4-native-rb:latest bundle exec ruby parse_test.rb
